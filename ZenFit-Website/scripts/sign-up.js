0ab73b77abd28b28fa953d9486568076a72d35cc
Matthew orr <123288277+MatthewbOrr@users.noreply.github.com>
2025-04-16 19:41:04 +1200

    second comit
    pop up menus and picture shadows
    
    # Conflicts:
    #	ZenFit-Website/pages.html/index.html
    #	ZenFit-Website/pages.html/sign-up.html
    #	ZenFit-Website/styles/core-style.css
    #	ZenFit-Website/styles/homepage.css
    #	about-us.html

---
+++ ZenFit-Website/scripts/sign-up.js
@@ -0,0 +1,26 @@
+document.getElementsByClassName("popup1");
+
+    function openPopup1() {
+        popup.classList.add("open-popup1");
+    }
+    function closePopup1() {
+        popup.classList.remove("open-popup1");
+    }
+
+
+document.getElementsByClassName("popup2");
+
+    function openPopup2() {
+        popup2.classList.add("open-popup2");
+    }
+    function closePopup2() {
+        popup2.classList.remove("open-popup2");
+    }
+
+
+
+
+
+
+
+    
